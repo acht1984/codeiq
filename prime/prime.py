b1b01ca05b886e95e1df3a9bb40b12cee5af5bf9
Yamaki Hiroshi <hiroshi.yamaki@gmail.com>
2015-02-24 01:11:13 +0900

    処理速度の改善

--- prime/prime.py
+++ prime/prime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #encoding: utf-8
+from math import log, ceil
 
 def sum_nums(num):
     result = 0
@@ -9,20 +10,22 @@ def sum_nums(num):
     return result
 
 def primes(num):
-    result = [2]
-    # 2以外の素数はすべて奇数
-    for i in range(3, num + 1, 2):
-        # それまでの素数で割れなければiは素数
-        if reduce(lambda x, y: x and i % y != 0, result, True):
-            result.append(i)
+    result = []
+    targets = range(2, num + 1)
+    while len(targets):
+        prime = targets[0]
+        result.append(prime)
+        targets = filter(lambda x: x % prime, targets)
     return result
 
-def devide_primes(num, primes):
+def devide_primes(num, primes, wa):
     result = []
     for prime in primes:
         while num % prime == 0: 
             result.append(prime)
             num /= prime
+        if num == 1:
+            return result
     return result
 
 
@@ -31,9 +34,13 @@ if __name__ == '__main__':
     # 解には少なくとも一つの積があり、その最小値は2
     # そのため素数リストの終端値を予め1/2する
     primes = primes(target / 2)
+    # かけるの数も2の倍数の時が最大
+    limit = int(log(target, 2))
     for num in range(1000, target):
         wa = sum_nums(num)
-        soinsu = devide_primes(num, primes)
+        if wa > limit:
+            continue
+        soinsu = devide_primes(num, primes, wa)
         if wa == len(soinsu) - 1:
             print num, wa, soinsu
 
